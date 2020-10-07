72dd3d565f2b185379e030b74f8ce5d5c3921625
Riftan13 <55617983+Riftan13@users.noreply.github.com>
2020-10-07 12:36:08 +0700

    Add files via upload

---
+++ tugas4_2.php
@@ -0,0 +1,7 @@
+<?php
+$saluran = 523;
+$pakai = 8891;
+$rata = $pakai / $saluran;
+
+echo "Jumlah Saluran = $saluran <br> Jumlah terpakai = $pakai <br> Rata-rata Pemakaian = ....? <br> Jawaban : $pakai / $saluran = $rata liter/keluarga";
+?>
